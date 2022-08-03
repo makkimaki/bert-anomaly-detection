91c26886f4328767f763dc9a6d9778f62af27cdd
makkimaki <shellingford-93.4.2@hotmail.co.jp>
2022-08-03 13:31:34 +0000

    Add: BertSelfAttention

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
-from turtle import forward, position
 from torch import nn
 import torch
+import math
 
 
 class BertLayerNorm(nn.Module):
@@ -160,3 +160,84 @@ class BertAttention(nn.Module):
             self_output = self.selfattn(input_tensor, attention_mask, attention_show_flg)
             attention_output = self.output(self_output, input_tensor)
             return attention_output
+
+
+class BertSelfAttention(nn.Module):
+
+    def __init__(self, config) -> None:
+        super(BertSelfAttention).__init__()
+
+        self.num_attention_heads = config.num_attention_heads
+        # num_attention_heads: 12
+
+        self.attention_head_size = int(
+            config.hidden_size / config.num_attention_heads
+            )
+        self.all_head_size = self.num_attention_heads * \
+            self.attention_head_size  # = hidden_size: 768
+
+        # Q, K, V of self-attention
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+        
+        # Dropout
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+    
+    def transpose_for_scores(self, x):
+        """ Transforms input data for multi-head attention
+        [batch_size, seq_len, hidden] -> [batch_size, 12, seq_len, hidden/12]
+        """
+        new_x_shape = x.size()[
+            :-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states, attention_mask, attention_show_flg=False):
+        """
+        Args: 
+            hidden_states: 
+            attention_mask: 
+            attention_show_flg:
+        """
+        # 
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        # transforms the tensor for multi-head attention
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        # get the similarity between feature itself as Attention_scores
+        attention_scores = torch.matmul(
+            query_layer, key_layer.transpose(-1, -2)
+        )
+        attention_scores = attention_scores / \
+            math.sqrt(self.attention_head_size)
+
+        # seal with mask where the mask
+        ## after the normalization with softmax, mask becomes -inf.
+        ## attention_maskには 0 or -inf が元々入ってるため、足し算にしておく。
+        attention_scores = attention_scores * attention_mask
+
+        # normalization of Attention using softmax
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+
+        # dropout
+        attention_probs = self.dropout(attention_probs)
+
+        # 
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        # return multi-head attention tensor into ex-format
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[
+            :-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        if attention_show_flg:
+            return context_layer, attention_probs
+        elif attention_show_flg == False:
+            return context_layer
